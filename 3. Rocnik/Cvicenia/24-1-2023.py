50bdc2bfa71a2c50530f0da9b5cb44c14fd1fe91
aksel <114018219+Pablo-Diego-Jose-Francisco-De-Papa-II@users.noreply.github.com>
2023-01-25 20:06:26 +0100

    Update 24-1-2023.py

--- 3. Rocnik/Cvicenia/24-1-2023.py
+++ 3. Rocnik/Cvicenia/24-1-2023.py
@@ -21,5 +21,21 @@ def klik(key):
 
     ca.coords("line", sur)
 
-ca.bind_all("<Key>", klik)
-tk.mainloop()
+#ca.bind_all("<Key>", klik)
+
+
+#Zadanie: importni random png subor do platna a nech sa hybe wasd.
+obr, move = tk.PhotoImage(file = "notmiguel.png"), 10
+ca.create_image(250, 250, image = obr, tags = "gato")
+
+def kiten(key):
+    name = key.keysym
+
+    match name:
+        case "Right": ca.move("gato", move, 0)
+        case "Left":  ca.move("gato", -move, 0)
+        case "Up":    ca.move("gato", 0, -move)
+        case "Down":  ca.move("gato", 0, move)
+
+ca.bind_all("<Key>", kiten)
+ca.mainloop()
